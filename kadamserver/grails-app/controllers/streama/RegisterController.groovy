726c954f99a53385b7a5eedaac3485a711cab594
pkadam <pkadam@scan-it-pdc134.SCAN-IT-PDC>
2018-08-10 16:08:26 +0530

    register

--- kadamserver/grails-app/controllers/streama/RegisterController.groovy
+++ kadamserver/grails-app/controllers/streama/RegisterController.groovy
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.WebAttributes
 
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpSession
 import javax.servlet.http.Cookie
