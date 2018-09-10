9cb57d01406f277479b90402380d80f4b6dfd8e9
pkadam <pkadam@scan-it-pdc134.SCAN-IT-PDC>
2018-09-10 20:16:45 +0530

    bolt

--- kadamserver/grails-app/controllers/streama/RegisterController.groovy
+++ kadamserver/grails-app/controllers/streama/RegisterController.groovy
@@ -256,7 +256,7 @@ class RegisterController {
     def conf = getConf()
 
 	String postUrl = request.contextPath + '/register/register'
-    render view: 'registration', model: [postUrl: postUrl]
+    render view: 'boltregistration', model: [postUrl: postUrl]
     
     /** redirect(uri: '/#/register') */
   }
