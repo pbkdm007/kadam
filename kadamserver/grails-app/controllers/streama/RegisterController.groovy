ff3e3f0b5bf1682e92daa14db43093d778ad75bc
pkadam <pkadam@LAPTOP-437KDQAN>
2018-11-16 15:15:50 +0530

    changes

--- kadamserver/grails-app/controllers/streama/RegisterController.groovy
+++ kadamserver/grails-app/controllers/streama/RegisterController.groovy
@@ -148,7 +148,7 @@ class RegisterController {
 			
 			    user.save flush: true
 			
-			    UserRole.create(userInstance, Role.findByAuthority("ROLE_CONTENT_MANAGER"))
+			    UserRole.create(user, Role.findByAuthority("ROLE_CONTENT_MANAGER"))
 			    
 			    /**Cookie cookie = new Cookie("myCookie",username)
 				cookie.maxAge = -1
